0a88c3923284cee21fbaefd6fc0a0470aad83651
osamas19 <osama.dogorshom@gmail.com>
2023-02-04 22:20:44 +0000

    Director final

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -23,4 +23,13 @@ class DirectorsController < ApplicationController
     render ({ :template => "director_templates/youngest.html.erb"})
   end
 
+  def director_details
+
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id }).at(0)
+    @filmography = Movie.where({ :director_id => @the_director })
+    
+    render ({ :template => "director_templates/director_details.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
   
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
