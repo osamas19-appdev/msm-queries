ac069cbad77c78634b92f59b447ad7499be088b0
osamas19 <osama.dogorshom@gmail.com>
2023-02-04 21:39:33 +0000

    Directors eldest youngest

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
 end
